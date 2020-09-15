36cf85e4bebeb85b5ac12b2a194b0b30a03fd645
Kevin Paul Montealegre Melo <kmontealegremelo@gmail.com>
2020-09-14 19:19:05 -0500

    Initial Commit

---
+++ StartProyect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StartProyect
+//
+//  Created by general on 14/09/20.
+//  Copyright © 2020 general. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
